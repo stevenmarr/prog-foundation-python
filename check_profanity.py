0b3e1a06db5e944a3d1a474939e5cd3727c17140
Steven Marr <stevenmarr@me.com>
2015-01-10 19:26:35 -0700

    Add profanity check

--- check_profanity.py
+++ check_profanity.py
@@ -1,7 +1,14 @@
+import urllib
+
 def read_text():
-    quotes = open("/users/stevenmarr/prog-found-python")
-    contents_of_file = quotes.read()
-    print(contents_of_file)
+    quotes = open("/users/stevenmarr/prog-found-python/movie_quotes.txt")
+    check_profanity(quotes.read())
     quotes.close()
-    
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+
+    text_to_check)
+    print(connection.read())
+    connection.close()
+
+
 read_text()
