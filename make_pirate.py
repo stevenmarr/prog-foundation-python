040e8706f7c978560b1d1d8490f5dbf4debf0a7b
Steven Marr <stevenmarr@me.com>
2015-01-10 21:52:12 -0700

    Change file open error output

--- make_pirate.py
+++ make_pirate.py
@@ -2,11 +2,11 @@ import urllib
 
 def open_file():
     try:
-        file=open("/users/stevenmarr/prog-found-python/speech.txt")
+        file=open("/dusers/stevenmarr/prog-found-python/speech.txt")
         text = file.read()
         file.close()
         return(text)
-    except: return("Bad file name")
+    except: return("Grrr someting broke mate!")
 
 
 def make_pirate_speech():
